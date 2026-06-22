a3c689db73103a1b086ed50536c3c4b0487dcf8d
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-22 16:24:13 +0200

    finisc class

--- src/main/java/robertovisconti/entities/Tessera.java
+++ src/main/java/robertovisconti/entities/Tessera.java
@@ -3,6 +3,7 @@ package robertovisconti.entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -11,8 +12,51 @@ public class Tessera {
 
     @Id
     @GeneratedValue
-    @Column
+    @Column(name = "id_tessera")
     private UUID id;
 
+    @Column(name = "data_emissione", nullable = false)
+    private LocalDate dataEmissione;
 
+    @Column(name = "data_scadenza", nullable = false)
+    private LocalDate dataScadenza;
+
+
+    @Column(name = "codice_univoco", nullable = false, unique = true)
+    private UUID codiceUnivoco;
+
+    protected Tessera() {
+    }
+
+    public Tessera(UUID codiceUnivoco) {
+        this.dataEmissione = LocalDate.now();
+        this.dataScadenza = this.dataEmissione.plusYears(1);
+        this.codiceUnivoco = codiceUnivoco;  // UUID.randomUUID(); alla creazione tessera
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public LocalDate getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public UUID getCodiceUnivoco() {
+        return codiceUnivoco;
+    }
+
+    @Override
+    public String toString() {
+        return "Tessera{" +
+                "id=" + id +
+                ", dataEmissione=" + dataEmissione +
+                ", dataScadenza=" + dataScadenza +
+                ", codiceUnivoco=" + codiceUnivoco +
+                '}';
+    }
 }
