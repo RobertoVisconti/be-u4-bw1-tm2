05bfa7fa12b8f67b94c1900aeaf667df1b69c805
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-22 15:40:39 +0200

    Initial commit

---
+++ src/main/java/robertovisconti/Application.java
@@ -0,0 +1,12 @@
+package robertovisconti;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("be-u4-bw1-tm2");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
