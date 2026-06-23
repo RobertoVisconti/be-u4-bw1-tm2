a12e8981abab1b8d2d29e6d793b101ee470a054b
alessandro.nappa <alessandro01nappa@gmail.com>
2026-06-23 12:06:49 +0200

    Aggiunto generatore di mezzi di trasporto, blocco da 20

--- src/main/java/robertovisconti/Application.java
+++ src/main/java/robertovisconti/Application.java
@@ -4,12 +4,17 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import net.datafaker.Faker;
+import robertovisconti.dao.MezzoDiTrasportoDAO;
 import robertovisconti.dao.TesseraDAO;
 import robertovisconti.dao.UtenteDAO;
+import robertovisconti.entities.MezzoDiTrasporto;
 import robertovisconti.entities.Tessera;
 import robertovisconti.entities.Utente;
 import robertovisconti.enums.Ruolo;
+import robertovisconti.enums.StatoMezzo;
+import robertovisconti.enums.TipoMezzo;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class Application {
@@ -45,6 +50,34 @@ public class Application {
         }
 
         System.out.println("Generazione dei 50 utenti iniziali completata.");
+
+        // creo il DAO dei mezzi
+        MezzoDiTrasportoDAO mezzoDAO = new MezzoDiTrasportoDAO(em);
+
+        // mi servono i valori degli enum e un Random
+        TipoMezzo[] tipi = TipoMezzo.values();
+        StatoMezzo[] stati = StatoMezzo.values();
+        Random random = new Random();
+
+        // genero 20 mezzi di trasporto
+        for (int i = 0; i < 20; i++) {
+
+            // scelgo a caso il tipo (BUS o TRAM) e lo stato (IN_SERVIZIO o IN_MANUTENZIONE)
+            TipoMezzo tipo = tipi[random.nextInt(tipi.length)];
+            StatoMezzo stato = stati[random.nextInt(stati.length)];
+
+            // capienza tra 30 e 200 posti
+            int capienza = random.nextInt(30, 201);
+
+            // targa finta generata da Faker 
+            String targa = faker.vehicle().licensePlate();
+
+            // creo il mezzo e lo salvo nel database
+            MezzoDiTrasporto mezzo = new MezzoDiTrasporto(tipo, capienza, stato, targa);
+            mezzoDAO.save(mezzo);
+        }
+
+        System.out.println("Generazione dei 20 mezzi di trasporto completata.");
 
         em.close();
         entityManagerFactory.close();
