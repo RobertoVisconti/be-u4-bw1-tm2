7e0e0eea3056f75d22460d6476750445ef118e0f
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-23 10:14:01 +0200

    add class utente dao

--- src/main/java/robertovisconti/dao/UtenteDAO.java
+++ src/main/java/robertovisconti/dao/UtenteDAO.java
@@ -1,6 +1,45 @@
 package robertovisconti.dao;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import robertovisconti.entities.Utente;
+import robertovisconti.exceptions.UtenteNonTrovatoException;
+
+import java.util.UUID;
+
+
 public class UtenteDAO {
+
+    private final EntityManager em;
+
+    public UtenteDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void saveUtente(Utente utente) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(utente);
+        tx.commit();
+        System.out.println(utente + " Utente salvato con successo.");
+    }
+
+    public Utente findByID(UUID id) {
+        Utente utente = em.find(Utente.class, id);
+        if (utente == null) {
+            throw new UtenteNonTrovatoException(id);
+        }
+        return utente;
+    }
+
+    public void deleteUtente(UUID id) {
+        Utente utente = findByID(id);
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.remove(utente);
+        tx.commit();
+        System.out.println(utente + " Rimosso con successo.");
+    }
 
 }
