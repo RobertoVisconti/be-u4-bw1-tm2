7607befbe55597596502d45bce7c0648785bce5d
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-24 12:16:23 +0200

    add menu utente e viaggio

--- src/main/java/robertovisconti/Application.java
+++ src/main/java/robertovisconti/Application.java
@@ -54,7 +54,7 @@ public class Application {
                 switch (emailScanner.getRuolo()) {
                     case ADMIN ->
                             caseAdmin(tesseraDAO, utenteDAO, mezzoDiTrasportoDAO, puntoDiEmissioneDAO, trattaDAO, percorrenzaDAO);
-                    case USER -> caseUser(tesseraDAO);
+                    case USER -> caseUser(tesseraDAO, puntoDiEmissioneDAO, trattaDAO);
                     default -> System.out.println("Ruolo non riconosciuto.");
                 }
 
@@ -111,7 +111,7 @@ public class Application {
     }
 
     //Case Utente
-    public static void caseUser(TesseraDAO tesseraDAO) {
+    public static void caseUser(TesseraDAO tesseraDAO, PuntoDiEmissioneDAO puntoDiEmissioneDAO, TrattaDAO trattaDAO) {
         boolean userMenu = true;
         while (userMenu) {
             System.out.println("\n MENU PRINCIPALE UTENTE");
@@ -129,7 +129,13 @@ public class Application {
             }
 
             switch (scelta) {
-                case 1 -> System.out.println("Funzionalità utente in sviluppo...");
+                case 1 -> {
+                    PuntoDiEmissione punto = selezionaPunto(puntoDiEmissioneDAO);
+                    if (punto != null) {
+                        casePunto(punto);
+                    }
+                }
+                case 2 -> caseViaggio(trattaDAO);
                 case 0 -> {
                     System.out.println("Logout utente effettuato.");
                     userMenu = false;
@@ -140,7 +146,7 @@ public class Application {
     }
 
     // Case Punto Vendita
-    public static void casePunto() {
+    public static void casePunto(PuntoDiEmissione punto) {
         boolean puntoMenu = true;
         while (puntoMenu) {
             System.out.println("\n MENU PUNTO VENDITA");
@@ -164,7 +170,41 @@ public class Application {
                 case 2 -> System.out.println("2. Compra abbonamento");
                 case 3 -> System.out.println("3. Rinnova tessera");
                 case 4 -> System.out.println("4. Rinnova Abbonamento");
-                case 0 -> System.out.println("0. Torna al menu principale");
+                case 0 -> {
+                    System.out.println("Torno al menu principale utente");
+                    puntoMenu = false;
+                }
+                default -> System.out.println("Opzione non valida.");
+            }
+
+        }
+    }
+
+    // Case Viaggio
+    public static void caseViaggio(TrattaDAO trattaDAO) {
+        boolean viaggioMenu = true;
+
+        while (viaggioMenu) { // Il ciclo inizia qui
+            System.out.println("\n MENU VIAGGI");
+            System.out.println("1. Scegli Viaggio");
+            System.out.println("0. Torna al menu principale");
+            System.out.print("Scegli un'opzione: ");
+
+            int scelta;
+            try {
+                scelta = Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException ex) {
+                System.out.println("Errore: Inserire un numero valido.");
+                scelta = -1;
+            }
+
+            switch (scelta) {
+                case 1 -> selezionaTratta(trattaDAO);
+                case 0 -> {
+                    System.out.println("Torno al menu principale utente");
+                    viaggioMenu = false;
+                }
+                default -> System.out.println("Opzione non valida.");
             }
         }
     }
